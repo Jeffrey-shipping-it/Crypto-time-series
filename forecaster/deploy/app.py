2b096c967a421119d5b55d5c373caca122a3a020
jong653 <jeffrey.dejong@kpn.com>
2021-06-09 08:27:31 +0200

    Fix routing to /predict endpoint

--- forecaster/deploy/app.py
+++ forecaster/deploy/app.py
@@ -5,13 +5,35 @@ import os
 import argparse
 import logging
 import json
+from forecaster.utils import _load_model_dict
 
 app = Flask(__name__)
+
+
+def forecast(ticker, data):
+    models = _load_model_dict()
+    print(models)
+    model = models[ticker]['model']
+    future = model.make_future_dataframe(data, periods=8, n_historic_predictions=False)
+    forecast = model.predict(future)
+    lookup_df = forecast.drop(['ds'], axis=1)
+    projection = forecast.assign(y_pred=lookup_df.lookup(lookup_df.index, lookup_df.isnull().idxmin(1)))[
+        ['ds', 'y_pred']]
+    return projection
 
 
 @app.route('/')
 def home():
     return True
+
+
+@app.route('/predict/', methods=['POST'])
+def predict():
+    payload = request.get_json(force=True)
+    ticker = payload['ticker']
+    data = payload['data']
+    projection = forecast(ticker, data)
+    return projection.to_json()
 
 
 if __name__ == "__main__":
@@ -25,23 +47,3 @@ if __name__ == "__main__":
     config = json.load(open(args.config))
 
     app.run(debug=True)
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    payload = request.get_json(force=True)
-    ticker = payload['ticker']
-    data = payload['data']
-    projection = forecast(ticker, data)
-    return projection.to_json()
-
-
-def forecast(ticker, data):
-    models = pickle.load(open('models.pickle', 'rb'))
-    model = models[ticker]['model']
-    future = model.make_future_dataframe(data, periods=8, n_historic_predictions=False)
-    forecast = model.predict(future)
-    lookup_df = forecast.drop(['ds'], axis=1)
-    projection = forecast.assign(y_pred=lookup_df.lookup(lookup_df.index, lookup_df.isnull().idxmin(1)))[
-        ['ds', 'y_pred']]
-    return projection
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='crypto-time-series', version='1.0', packages=find_packages())
